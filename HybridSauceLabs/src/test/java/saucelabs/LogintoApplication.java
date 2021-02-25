980aa7f6b65fab06945ca809e74882ad2a39e694
Sharathk1962 <sharath.k1991@gmail.com>
2021-02-26 00:19:42 +0530

    cleaned testng multiple parameters

--- HybridSauceLabs/src/test/java/saucelabs/LogintoApplication.java
+++ HybridSauceLabs/src/test/java/saucelabs/LogintoApplication.java
@@ -3,16 +3,18 @@ package saucelabs;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.beust.jcommander.Parameter;
 
 import resources.BaseGeneric;
 
 public class LogintoApplication extends BaseGeneric {
 	
-
-	@Test
 	public void logintosauce() throws IOException
 	{
+		
 	driver=initialisedriver();
 	}
 

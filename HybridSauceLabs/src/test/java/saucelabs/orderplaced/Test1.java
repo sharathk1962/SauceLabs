047f6f2b9f5216378b82797afa0b9342b5916e10
Sharathk1962 <sharath.k1991@gmail.com>
2021-04-22 22:28:23 +0530

    new tst1 file

---
+++ HybridSauceLabs/src/test/java/saucelabs/orderplaced/Test1.java
@@ -0,0 +1,16 @@
+package saucelabs.orderplaced;
+
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		int b=2;
+		int c=3;
+		int d=a+b+c;
+
+	}
+
+}

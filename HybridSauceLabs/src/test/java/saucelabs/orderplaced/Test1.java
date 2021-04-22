aef44d826a10e2274f3e498c4e57704157e3d55c
Sharathk1962 <sharath.k1991@gmail.com>
2021-04-22 23:53:34 +0530

    juss

--- HybridSauceLabs/src/test/java/saucelabs/orderplaced/Test1.java
+++ HybridSauceLabs/src/test/java/saucelabs/orderplaced/Test1.java
@@ -7,9 +7,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=1;
-		int d=3;
-		int ne=8;
-		int e=a+d+ne;
+		
+		int ned=8;
+		int e=a+ned;
 
 
 	}
